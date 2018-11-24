0718e996401ddba1b1f0a36d8156663355869b48
Julio <julio@perfilan.com>
2018-11-24 13:49:10 -0700

    yo

--- pages/info.js
+++ pages/info.js
@@ -1,10 +1,34 @@
 export default class extends React.Component {
-    render(){
+    render() {
         return <div>
-            <h1>Hola Mundo</h1>
-            <p>Hola Vida</p>
+            <img src="/static/platzi-logo.png" />>
+            <h1>Creado por Julio</h1>
+            <p>Curso de Next.JS de Platzi</p>
+
+            <style>{`
+
+            div {
+                text-align: center;
+                margin-top:10%
+            }
+
+            h1 {
+                color: white;
+            }
+
+            p {
+                color: white;
+            }
+
+            img {
+                max-width: 50%;
+            }
+
+            `}</style>
             <style jsx global>{`
-           
+                body {
+                    background: #1c3643;
+                }
             `}</style>
         </div>
     }
