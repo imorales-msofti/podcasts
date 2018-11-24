4a07420b494ad71571f9a7e9d5128a59bf6261ea
Julio <julio@perfilan.com>
2018-11-23 18:58:15 -0700

    Hola Mundo Next.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default class extends React.Component {
+    render(){
+        return <div>
+            <h1>Hola Mundo</h1>
+            <p>Bienenidos al Curso de Next.js</p>
+        </div>
+    }
+}
