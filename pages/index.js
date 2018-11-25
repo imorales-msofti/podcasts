2bcb4a5f162b5f8b40e9401be592c4f82696c507
Julio <julio@perfilan.com>
2018-11-24 19:37:03 -0700

    fetch con isomorphic-fetch

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,49 @@
+import fetch from 'isomorphic-fetch'
+
 export default class extends React.Component {
+
+    // getInitialProps() es una función que solo se puede ejecutar en Next.js 
+    
+    static async getInitialProps(){
+        
+        // Llamo al API de audioboom y obtengo la los caneles recomendados 
+        let req = await fetch
+        ('https://api.audioboom.com/channels/recommended')
+
+        //  obtengo todo lo que contiene "body": [] y lo asigno a una variable llamada channels
+        let { body: channels } = await req.json()
+
+        // Regreso los todos los channels
+        console.log(channels)
+        return { channels }
+    }
+
     render(){
+        const { channels } = this.props
         return <div>
-            <h1>Hola Mundo</h1>
-            <p>Bienenidos al Curso de Next.js</p>
-            <img src="/static/platzi-logo.png" alt="Platzi"/>
+            <header>Podcasts</header>
+
+            {
+                channels.map((channel) => (
+                    <div>{ channel.title } </div>
+
+                ))
+            }
             
             <style jsx>{`
-            h1 {
-                color: red;
+            header {
+                color: #fff;
+                background: #8756ca;
+                padding: 15px;
+                text-align: center;
             }
-            :global(div p) {
-                color: green;
-            }
-            img {
-                max-width: 50%;
-                display: block;
-                margin: 0 auto;
-            }
+           
             `}</style>
 
             <style jsx global>{`
             body {
+                margin: 0;
+                font-family: system-ui
                 background: white;
             }
             `}</style>
