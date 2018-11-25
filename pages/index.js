f9ff6d519d5d636d86d5a61e8248c6819cf7e31b
Julio <julio@perfilan.com>
2018-11-24 20:52:18 -0700

    utilizar grid

--- pages/index.js
+++ pages/index.js
@@ -20,21 +20,52 @@ export default class extends React.Component {
 
     render(){
         const { channels } = this.props
+
         return <div>
             <header>Podcasts</header>
 
+            <div className="channels">
             {
                 channels.map((channel) => (
-                    <div>{ channel.title } </div>
-
+                    <a className="channel">
+                    <img src={ channel.urls.logo_image.original } alt="" />
+                    <h2>{ channel.title }</h2>
+                    </a>
                 ))
             }
+            </div>
             
             <style jsx>{`
             header {
                 color: #fff;
                 background: #8756ca;
                 padding: 15px;
+                text-align: center;
+            }
+            .channels {
+                display: grid;
+                grid-gap: 15px;
+                padding: 15px;
+                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+            }
+            .channel {
+                display: block;
+                border-radius: 3px;
+                margin-bottom: 0.5em;
+                color: #333;
+                text-decoration: none;
+            }
+            .channel img {
+                width: 100%;
+                border-radius: 3px;
+                box-shadow: 0px 2px 6px rgba(0,0,0,0,.15);
+                width: 100px;
+            }
+            h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                maring: 0;
                 text-align: center;
             }
            
