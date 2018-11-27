41aa6e32d582f82c4679b2c65c53e53f79e9648c
Julio <julio@perfilan.com>
2018-11-26 21:07:56 -0700

    channel fetch

--- pages/channel.js
+++ pages/channel.js
@@ -9,26 +9,65 @@ export default class extends React.Component {
         let idChannel = query.id
         console.log(idChannel)
         
-        let req = await fetch(`https://api.audioboom.com/channels/${idChannel}`)
-        
-        // Se escribe body: { channel }, por que el objeto dentro del json tiene un nombre así: channel:
-        let { body: { channel } } = await req.json()
+        /*
+        1.- FORMA DE HACERLO
+        let reqChannel = await fetch(`https://api.audioboom.com/channels/${idChannel}`)
+        let dataChannel = await reqChannel.json()
+        let channel = dataChannel.body.channel
+
+        let reqAudios = await fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+        let dataAudios = await reqAudios.json()
+        let audioClips = dataAudios.body.audio_clips
 
         console.log(channel)
-        return { channel }
+        console.log(audioClips)
+
+        return { channel, audioClips }
+        */
+
+        /*
+        2.- FORMA DE HACERLO
+        */
+       
+        let [reqChannel, reqSeries, reqAudios] = await Promise.all([
+            fetch(`https://api.audioboom.com/channels/${idChannel}`),
+            fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+            fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`),
+        ])
+
+        let dataChannel = await reqChannel.json()
+        let channel = dataChannel.body.channel
+
+        let dataAudios = await reqAudios.json()
+        let audioClips = dataAudios.body.audio_clips
+
+        let dataSeries = await reqSeries.json()
+        let series = dataSeries.body.channels
+
+        return { channel, audioClips, series}
+       
     }
 
     render() {
-        const { channel } = this.props
+        const { channel, audioClips, series } = this.props
         return <div>
             <header>Podcasts</header>
+            <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
             <h1>{ channel.title }</h1>
+
             <style jsx>{`
             header {
                 color: #fff;
                 background: #8756ca;
                 padding: 15px;
                 text-align: center;
+            }
+            .banner {
+                width: 100%;
+                padding-bottom: 25%;
+                background-position: 50% 50%;
+                background-size: cover;
+                background-color: #aaa;
             }
             .channels {
                 display: grid;
